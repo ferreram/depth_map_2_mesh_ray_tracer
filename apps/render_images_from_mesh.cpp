dd82a5a527a22628aa48613e69f74cbea2d9c182
Maxime Ferrera <maxime.ferrera@ifremer.fr>
2022-09-27 12:08:47 +0200

    Image rendering from mesh added.

--- apps/render_images_from_mesh.cpp
+++ apps/render_images_from_mesh.cpp
@@ -6,22 +6,27 @@
 
 int main(int argc, char *argv[])
 {
-  if (argc < 4)
+  if (argc < 5)
   {
-    std::cerr << "Usage : ./ply_mesh_to_depth_maps mesh_path colmap_result_path out_depth_maps_path filter_from_normals(default = true)" << std::endl;
+    std::cerr << "Usage : ./render_images_from_mesh mesh_path mesh_tex_img_path colmap_result_path out_rendered_img_path save_as_png(default = true) filter_from_normals(default = true)" << std::endl;
     return -1;
   }
 
   // 1. Get args
   // =============================================
   std::string mesh_path = argv[1];
-  std::string colmap_result_path = argv[2];
-  std::string out_depth_maps_path = argv[3];
+  std::string mesh_tex_img_path = argv[2];
+  std::string colmap_result_path = argv[3];
+  std::string out_rendered_img_path = argv[4];
 
+  bool save_as_png = true;
   bool filter_from_normals = true;
-  if (argc > 4)
+  if (argc > 5)
   {
-    filter_from_normals = std::stoi(argv[4]);
+    save_as_png = std::stoi(argv[5]);
+
+    if (argc > 6)
+      filter_from_normals = std::stoi(argv[6]);
   }
 
   // 2. Load Camera's Calib (Expecting Undistorted Calib) and Images' SE(3) Poses
@@ -40,13 +45,15 @@ int main(int argc, char *argv[])
   // 4. Load mesh file and Init BVH-tree + Raycast every images onto Mesh
   // + Save the resulting Depth Maps!
   // ====================================================================
-  RaycastImagesToMesh(mesh_path, 
-                      out_depth_maps_path, 
+  RenderImagesFromMesh(mesh_path, 
+                      mesh_tex_img_path,
+                      out_rendered_img_path, 
                       colmap_reader.m_map_img_names_T_world_cam, 
                       image_rays.m_vbvs, 
                       image_rays.m_img_cols, 
                       image_rays.m_img_rows,
-                      filter_from_normals);
+                      filter_from_normals,
+                      save_as_png);
 
   return 0;
 }
--- include/mesh_loader.h
+++ include/mesh_loader.h
@@ -101,3 +101,115 @@ void readPlyFile(const std::string &mesh_path, std::vector<float3> &vverts, std:
         std::cerr << "Caught tinyply exception: " << e.what() << std::endl;
     }
 }
+
+
+void readPlyFile(const std::string &mesh_path, std::vector<float3> &vverts, std::vector<uint3> &vfaces,
+                std::vector<float3> &vnormals, std::vector<float6> &vtexuv)
+{
+    std::cout << "........................................................................\n";
+    std::cout << "Reading PLY file: " << mesh_path << std::endl;
+
+    std::unique_ptr<std::istream> file_stream;
+    std::vector<uint8_t> byte_buffer;
+
+    manual_timer timer;
+    timer.start();
+
+    try
+    {
+        file_stream.reset(new std::ifstream(mesh_path, std::ios::binary));
+
+        if (!file_stream || file_stream->fail()) throw std::runtime_error("file_stream failed to open " + mesh_path);
+
+        file_stream->seekg(0, std::ios::end);
+        file_stream->seekg(0, std::ios::beg);
+
+        tinyply::PlyFile file;
+        file.parse_header(*file_stream);
+
+        std::cout << "\t[ply_header] Type: " << (file.is_binary_file() ? "binary" : "ascii") << std::endl;
+        for (const auto & c : file.get_comments()) std::cout << "\t[ply_header] Comment: " << c << std::endl;
+        for (const auto & c : file.get_info()) std::cout << "\t[ply_header] Info: " << c << std::endl;
+
+        for (const auto & e : file.get_elements())
+        {
+            std::cout << "\t[ply_header] element: " << e.name << " (" << e.size << ")" << std::endl;
+            for (const auto & p : e.properties)
+            {
+                std::cout << "\t[ply_header] \tproperty: " << p.name << " (type=" << tinyply::PropertyTable[p.propertyType].str << ")";
+                if (p.isList) std::cout << " (list_type=" << tinyply::PropertyTable[p.listType].str << ")";
+                std::cout << std::endl;
+            }
+        }
+
+        std::shared_ptr<tinyply::PlyData> pvertices, pfaces, pnormals, ptexcoords;
+
+        // The header information can be used to programmatically extract properties on elements
+        // known to exist in the header prior to reading the data. For brevity of this sample, properties 
+        // like vertex position are hard-coded: 
+        try { pvertices = file.request_properties_from_element("vertex", { "x", "y", "z" }); }
+        catch (const std::exception & e) { std::cerr << "tinyply exception: " << e.what() << std::endl; }
+
+        try { pfaces = file.request_properties_from_element("face", { "vertex_indices" }, 3); }
+        catch (const std::exception & e) { std::cerr << "tinyply exception: " << e.what() << std::endl; }
+
+        try { pnormals = file.request_properties_from_element("vertex", { "nx", "ny", "nz" }); }
+        catch (const std::exception & e) { std::cerr << "tinyply exception: " << e.what() << std::endl; }
+        
+        try { ptexcoords = file.request_properties_from_element("face", { "texcoord" }, 6); }
+        catch (const std::exception & e) { std::cerr << "tinyply exception: " << e.what() << std::endl; }
+
+        file.read(*file_stream);
+
+        if (pvertices)   std::cout << "\tRead " << pvertices->count  << " total vertices "<< std::endl;
+        if (pnormals) std::cout << "\tRead " << pnormals->count  << " total normals "<< std::endl;
+        if (pfaces)   std::cout << "\tRead " << pfaces->count  << " total faces "<< std::endl;
+        if (ptexcoords)   std::cout << "\tRead " << ptexcoords->count  << " total tex coords "<< std::endl;
+
+        if (pfaces->count != ptexcoords->count)
+        {
+            std::cerr << "\tSHOULD HAVE AS MANY TEXTURE COORDS AS FACES!!!\n";
+            std::cerr << "\tCHECK YOUR PLY FILE...\n";
+            exit(-1);
+        }
+        
+        const size_t numVerticesBytes = pvertices->buffer.size_bytes();
+        vverts.resize(pvertices->count);
+        std::memcpy(vverts.data(), pvertices->buffer.get(), numVerticesBytes);
+        
+        const size_t numFacesBytes = pfaces->buffer.size_bytes();
+        vfaces.resize(pfaces->count);
+        std::memcpy(vfaces.data(), pfaces->buffer.get(), numFacesBytes);
+
+        const size_t numTexBytes = ptexcoords->buffer.size_bytes();
+        vtexuv.resize(ptexcoords->count);
+        std::memcpy(vtexuv.data(), ptexcoords->buffer.get(), numTexBytes);
+
+        if (pnormals)
+        {
+            if (pvertices->count != pnormals->count)
+            {
+                std::cerr << "\tSHOULD HAVE AS MANY NORMALS AS VERTICES!!!\n";
+                std::cerr << "\tCHECK YOUR PLY FILE...\n";
+                exit(-1);
+            }
+
+            const size_t numNormalsBytes = pnormals->buffer.size_bytes();
+            vnormals.resize(pnormals->count);
+            std::memcpy(vnormals.data(), pnormals->buffer.get(), numNormalsBytes);
+
+            std::cout << "\n > Number of normals : " << vnormals.size() << std::endl;
+        }
+
+        std::cout << "\n > Number of vertices : " << vverts.size() << std::endl;
+        std::cout << "> Number of faces : " << vfaces.size() << std::endl;
+        std::cout << "> Number of tex coords : " << vtexuv.size() << std::endl;
+
+        timer.stop();
+        std::cout << "\n >>> PLY file " << mesh_path << " read in " << timer.get() << "ms!\n";
+    }
+    catch (const std::exception & e)
+    {
+        std::cerr << "Caught tinyply exception: " << e.what() << std::endl;
+    }
+}
